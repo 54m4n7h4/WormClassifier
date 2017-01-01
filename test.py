6c2e4e75c75a8748bed9ce0b23771d68bf29f810
Ryan Collins <ryan@localhost.localdomain>
2017-01-01 20:08:39 +0000

    Beginning of tests with hough lines

--- test.py
+++ test.py
@@ -17,8 +17,19 @@ if not img is None:
 
     thresholded_img = Thresholder(reducedNoise).binary_threshold_inverse(50,255)
 
+    cannygfp = cv2.Canny(thresholded_img.read_bright_field(),50,200,3)
+    outputimg = np.zeros_like(cannygfp)
+    min_line_length = 5
+    max_line_gap = 20
+    lines = cv2.HoughLinesP(cannygfp,1,np.pi/45,20,min_line_length,max_line_gap)
+
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        print(x1, ",",y1," to ",x2,",",y2)
+        cv2.line(outputimg,(x1,y1),(x2,y2), (255,255,255),1)
+
     # Output the bright fields
     cv2.imwrite("normalised.jpg", norm.read_gfp())
-    cv2.imwrite("output.jpg",thresholded_img.read_gfp())
+    cv2.imwrite("output.jpg",outputimg)
 else:
     print("No image file loaded")
